062d786150da4fbd52b9916f177165557bb868c5
kd3-80123 <kalambate456@gmail.com>
2024-05-28 10:58:28 +0530

    First React app

--- App.js
+++ App.js
@@ -1,15 +1,78 @@
-const heading = React.createElement("div",{ id: "parent"},
-[
-    React.createElement("div",{id:"child1"},
-[ React.createElement("h1",{},"In the h1 tag"),
-React.createElement("h2",{},"Inside the h2 tag"),
-]),
-React.createElement("div",{id:"child2"},
-[ React.createElement("h1",{},"In the h1 tag"),
-React.createElement("h2",{},"Inside the h2 tag"),
-])
+import React from "react";
+import ReactDom from "react-dom/client";
 
-]
-);
-        const root = ReactDOM.createRoot(document.getElementById("root"))
-        root.render(heading);y
+
+/****
+Header
+-logo
+-nav Items
+Body
+-search
+-Restarunt Container
+  - RestaruntCard
+    -Img
+    -Name of Rest. , Star Rating, Cuisine, delivery Time etc.
+Footer
+-CopyRight
+-Links
+-Address
+-Contact
+
+
+
+*/
+const Header = () =>{
+        return (
+                <div className="header">
+                <div>
+                        <img className="logo" src="https://cdn.dribbble.com/users/630677/screenshots/3833541/media/201454f743f48c415a38c49419275692.jpg?resize=768x576&vertical=center"></img>        
+                </div>
+                <div className="nav-items">
+                   <ul>
+                        <li>Home</li>
+                        <li>About Us</li>
+                        <li>Contact Us</li>
+                        <li>Cart</li>
+                   </ul>
+
+                </div>
+                </div>
+        );
+};
+
+const RestaruntCard = (props) =>{
+        const {resName, cuisine} = props;
+        return (
+                <div className="res-card">
+                  <img className="res-logo" alt="res-logo" src="https://b.zmtcdn.com/data/pictures/7/19100997/e6c6b1a506f9790ebb445d0d39452b53_o2_featured_v2.jpg?output-format=webp&fit=around|771.75:416.25&crop=771.75:416.25;*,*" />
+                  <h3>{resName}</h3>
+                  <h4>{cuisine}</h4>
+                  <h4>4.2 stars</h4>
+                  <h4>28 minutes</h4>
+                </div>
+        );
+};
+
+const Body = () =>{
+        return (
+                <div className="body">
+                   <div className="search"> Search</div>
+                   <div className="res-container">
+                   <RestaruntCard resName="Indori Sarafa" cuisine="North Indian Snacks" />
+                   <RestaruntCard resName="KFC" cuisine="Fried Chicken, Fast Food"/>
+
+                   </div>
+                </div>
+        )
+}
+
+const AppLayout = () => {
+        return (
+                <div className="app">
+                    <Header />
+                    <Body />
+                </div>
+        );
+};
+        const root = ReactDom.createRoot(document.getElementById("root"))
+        root.render( <AppLayout /> );
